7aaeec68e3b46b07015376092d57b9dfcfa33abd
18jwong <18jwong@shcp.edu>
2020-07-10 21:22:45 -0700

    Added Movement

---
+++ GMTK Real/Assets/Scripts/GhostMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+    public float speed = 2.0f; // Speed of movement
+    public LevelController levelController;
+
+    private Vector2 pos; // For movement
+    private bool moving = false;
+    
+    void Start () {
+        pos = transform.position; // Take the initial position
+    }
+
+    void FixedUpdate(){
+        if(Input.GetKey(KeyCode.A) && moving == false) {              // Left
+            moving = true;
+            pos += Vector2.left;
+            StartCoroutine("Move");
+        } else if(Input.GetKey(KeyCode.D) && moving == false) {       // Right
+            moving = true;
+            pos += Vector2.right;
+            StartCoroutine("Move");
+        } else if(Input.GetKey(KeyCode.W) && moving == false) {       // Up
+            moving = true;
+            pos += Vector2.up;
+            StartCoroutine("Move");
+        } else if(Input.GetKey(KeyCode.S) && moving == false) {       // Down
+            moving = true;
+            pos += Vector2.down;
+            StartCoroutine("Move");
+        }
+    }
+
+    IEnumerator Move(){
+        while( (Vector2)transform.position != pos ){
+            // Move there
+            transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);  
+            yield return null;
+        }
+        levelController.enqueueMove(pos);
+        moving = false;
+    }
+}
---
+++ GMTK Real/Assets/Scripts/LevelController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    private LinkedList<Vector2> moveQueue;
+
+    void Start(){
+        moveQueue = new LinkedList<Vector2>();
+    }
+
+    /*
+     * enqueueMove()
+     * adds a vector2 position to moveQueue
+     */
+    public void enqueueMove(Vector2 pos){
+        moveQueue.AddLast(pos);
+    }
+
+    /*
+     * dequeueMove()
+     * returns the vector2 at the beginning of the queue if it exists
+     *         null otherwise
+     */
+    public Vector2? dequeueMove(){
+        LinkedListNode<Vector2> v = moveQueue.First;
+
+        if(v == null){ // check if there are any moves queued up.
+            return null;
+        }
+
+        moveQueue.RemoveFirst();
+        return v.Value;
+    }
+
+}
