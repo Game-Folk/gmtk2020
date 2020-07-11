d63cfd0b68ed0e641d121004aed1b20fa75d7f7d
18jwong <18jwong@shcp.edu>
2020-07-10 17:02:26 -0700

    Started project

---
+++ GMTK Real/Assets/Scripts/SceneController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void loadScene(string s){
+        SceneManager.LoadScene(s);
+    }
+}
