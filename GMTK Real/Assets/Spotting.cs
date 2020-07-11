bcfe32926ec51539bb260fbb091959e1cda70334
John Chapman <johnfrancischapman@gmail.com>
2020-07-11 03:06:14 -0700

    commity

--- GMTK Real/Assets/Spotting.cs
+++ GMTK Real/Assets/Spotting.cs
@@ -1,41 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Spotting : MonoBehaviour
 {
 
-    public float playerspeed;
 
 
-    private Vector3 defaultScale;
-    private Vector3 defaultPos;
+  
 
     // Start is called before the first frame update
     void Start()
     {
-        defaultScale = transform.localScale;
-        //defaultPos = transform.position;
+       
     }   
 
     void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Wall")
-        {
-
-            transform.localScale -= new Vector3(playerspeed * Time.deltaTime * 3, 0, 0);
-            //transform.position -= new Vector3(playerspeed * Time.deltaTime, 0, 0);
-        }
+        
         if (collision.gameObject.name == "Monster")
         {
             Debug.Log("MONSTER SPOTTED");
+            Destroy(collision.gameObject);
+            GameObject button = new GameObject();
+            //button.AddComponent<Button>();
+           // button.transform.position = new Vector3(0,0,0);
+           // button.GetComponent<Button>().transform.localScale = new Vector3(3, 3, 3);
+           // button.GetComponent<Button>().onClick.AddListener(toMenu);
+            toMenu();
         }
     }
     void OnCollisionExit2D(Collision2D collision)
     {
-        transform.localScale = defaultScale;
-        //transform.position = defaultPos;
+       
         transform.rotation = Quaternion.Euler(0, 0, 0);
-        transform.position = new Vector3(transform.position.x + 1.8217f, 0.1936f, 0);
+    }
+
+    void toMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
