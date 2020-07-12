98f14e94296d21b5d5daa18ad64bad7c42981bda
18jwong <18jwong@shcp.edu>
2020-07-11 21:16:14 -0700

    end arrow

---
+++ GMTK Real/Assets/Scripts/EndLevelArrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevelArrow : MonoBehaviour
+{
+    public float checkTime = 0.1f;
+    public float checkDistance = 0.1f;
+    
+    [Header("Must Be Setup")]
+    public GameObject monster;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("DetectMonster");
+    }
+
+    IEnumerator DetectMonster(){
+        while( Vector3.Distance(transform.position, monster.transform.position) >= checkDistance ){
+            yield return new WaitForSeconds(checkTime);
+        }
+        // arrow touched so end level
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
